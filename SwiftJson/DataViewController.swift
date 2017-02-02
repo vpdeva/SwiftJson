016e82beda991b3d4f4b1f12f63ac8e27077ea74
imac <Caledonian@vishnu-devarajans-imac.local>
2017-02-02 16:27:30 +1300

    Initial Commit

---
+++ SwiftJson/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  SwiftJson
+//
+//  Created by Vishnu Prakash Devarajan on 2/02/17.
+//  Copyright © 2017 Vishnu Prakash Devarajan. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
